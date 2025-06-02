e7fcb153b4b314c50a9f0c1320bb7d4c5c8b8555
ramprakashpg <rampg23@gmail.com>
2025-06-01 23:06:24 -0400

    Configure kafka producer

---
+++ src/main/java/com/bd/Main.java
@@ -0,0 +1,42 @@
+package com.bd;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class Main {
+    public static volatile Main main;
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        Producer<String, String> producer = null;
+        try {
+            producer = new KafkaProducer<>(properties);
+            ProducerRecord<String, String> record = new ProducerRecord<>("Messages", "key", "value");
+            producer.send(record, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    if(e != null){
+                        System.out.println(e.getMessage());
+                    }else{
+                        System.out.println("Message sent successfully");
+                    }
+                }
+            });
+        } catch (Exception e) {
+            System.out.println("Exception Occurred:" + e.getMessage());
+        } finally {
+            if(producer != null){
+                producer.close();
+            }
+        }
+
+    }
+}
---
+++ src/main/java/com/bd/Producer.java
@@ -0,0 +1,16 @@
+package com.bd;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Producer {
+    private static final Logger logger = LoggerFactory.getLogger(Producer.class);
+    HashMap<String, String> hashMap = new HashMap<>();
+
+
+}
