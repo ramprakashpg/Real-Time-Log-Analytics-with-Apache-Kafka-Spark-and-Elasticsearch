a929b18d5f6c8094515060f8645249b8bb677dad
ramprakashpg <rampg23@gmail.com>
2025-06-02 21:04:59 -0400

    Refactor producer

--- src/main/java/com/bd/Main.java
+++ src/main/java/com/bd/Main.java
@@ -9,32 +9,5 @@ import java.util.Properties;
 public class Main {
     public static volatile Main main;
 
-    public static void main(String[] args) {
-        Properties properties = new Properties();
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        Producer<String, String> producer = null;
-        try {
-            producer = new KafkaProducer<>(properties);
-            ProducerRecord<String, String> record = new ProducerRecord<>("Messages", "key", "value");
-            producer.send(record, new Callback() {
-                @Override
-                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-                    if (e != null) {
-                        System.out.println(e.getMessage());
-                    } else {
-                        System.out.println("Message sent successfully");
-                    }
-                }
-            });
-        } catch (Exception e) {
-            System.out.println("Exception Occurred:" + e.getMessage());
-        } finally {
-            if (producer != null) {
-                producer.close();
-            }
-        }
 
-    }
 }
